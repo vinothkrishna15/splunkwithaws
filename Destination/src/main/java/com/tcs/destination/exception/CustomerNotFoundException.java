be7e1e718fd0c5dfcd527fb7379566fadbdf46e7
Muruganandam Nagalingam <muruganandam.nagalingam@tcs.com>
2015-03-02 15:32:42 +0530

    complete customer web services - updated code

--- Destination/src/main/java/com/tcs/destination/exception/CustomerNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.tcs.destination.exception;
-
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.http.HttpStatus;
-
-@ResponseStatus(value=HttpStatus.NOT_FOUND, reason="No such Customer")
-public class CustomerNotFoundException extends RuntimeException {
-
-}
--- Destination/src/main/java/com/tcs/destination/exception/UnAuthorizedException.java
+++
@@ -1,8 +0,0 @@
-package com.tcs.destination.exception;
-
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.http.HttpStatus;
-
-@ResponseStatus(value = HttpStatus.UNAUTHORIZED, reason = "Invalid Login")
-public class UnAuthorizedException extends RuntimeException {
-}
