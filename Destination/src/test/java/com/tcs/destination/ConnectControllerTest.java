acf769834f441ff2f93d43eb5a1f3867177a525c
Muruganandam Nagalingam <muruganandam.nagalingam@tcs.com>
2015-03-25 17:22:02 +0530

    junit for connect by date

--- Destination/src/test/java/com/tcs/destination/ConnectControllerTest.java
+++ Destination/src/test/java/com/tcs/destination/ConnectControllerTest.java
@@ -68,39 +68,9 @@ public class ConnectControllerTest {
 			mockMvc1=MockMvcBuilders.webAppContextSetup(ctx).addFilters(springSecurityFilterChain).build();
 		}
 		
-		/**
-		 * Test method for valid input{@link com.tcs.destination.controller.ConnectController#ConnectSearchById(java.lang.String)}.
-		 */
-		
+				
 		@Test
 		public void Test1ConnectById() throws Exception
-		{
-			mockMvc.perform(get("/connect/CNN3").accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk())
-				.andExpect(content().contentType(TestUtil.APPLICATION_JSON_UTF8))
-				.andExpect(jsonPath("$.connectId").value("CNN3"))
-				.andExpect(jsonPath("$.connectCategory").value("CUSTOMER"))
-				.andExpect(jsonPath("$.connectName").value("Cloud Connect"))
-				.andExpect(jsonPath("$.createdModifiedBy").value("734628"))
-				.andExpect(jsonPath("$.documentsAttached").value("NO"))
-//				.andExpect(jsonPath("$.createdModifiedDatetime").value("2015-02-01 09:45:12"))
-//				.andExpect(jsonPath("$.dateOfConnect").value("2015-02-20 09:35:42"))
-				.andExpect(jsonPath("$.customerMasterT.customerId").value("CUS542"))
-				.andExpect(jsonPath("$.customerMasterT.createdModifiedBy").value("287690"))
-				.andExpect(jsonPath("$.customerMasterT.customerName").value("ABN Amro EU"))
-				.andExpect(jsonPath("$.customerMasterT.documentsAttached").value("YES"))
-				.andExpect(jsonPath("$.customerMasterT.groupCustomerName").value("ABN Amro"))
-       			.andDo(print())
-				.andReturn();
-			ConnectT connect=connectService.searchforConnectsById("CNN3");
-				assertNotNull(connect);
-				assertEquals("2015-02-01 09:45:12.0", connect.getCreatedModifiedDatetime().toString());
-				assertEquals("2015-02-20 09:35:42.0", connect.getEndDatetimeOfConnect().toString());
-				
-		}
-		
-		@Test
-		public void Test2ConnectById() throws Exception
 		{
 			mockMvc.perform(get("/connect/CNN3?fields=connectId,connectCategory,connectName").accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
@@ -111,49 +81,17 @@ public class ConnectControllerTest {
 				.andDo(print())
 				.andReturn();
 		}
-		
-		
+				
 		
 		@Test
-		public void Test3ConnectById() throws Exception
+		public void Test2ConnectById() throws Exception
 		{
-			mockMvc.perform(get("/connect/CNN1").accept(MediaType.APPLICATION_JSON))
+			mockMvc.perform(get("/connect/CNN10").accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isNotFound());
 }
-		
-		/**
-		 * Junit Test case  {@link com.tcs.destination.controller.ConnectController#ConnectSearchByName(java.lang.String)}.
-		 */  
-		
-		
+				
 		@Test
 		public void Test1ConnectByName() throws Exception
-		{
-			mockMvc.perform(get("/connect?nameWith=Cloud").accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk())
-				.andExpect(content().contentType(TestUtil.APPLICATION_JSON_UTF8))
-			    .andExpect(jsonPath("$[0].connectId").value("CNN3"))
-			    .andExpect(jsonPath("$[0].connectCategory").value("CUSTOMER"))
-			    .andExpect(jsonPath("$[0].connectName").value("Cloud Connect"))
-			    .andExpect(jsonPath("$[0].createdModifiedBy").value("734628"))
-			    .andExpect(jsonPath("$[0].documentsAttached").value("NO"))
-//				.andExpect(jsonPath("$[0].createdModifiedDatetime").value("2015-01-20 15:40:35.0"))
-//				.andExpect(jsonPath("$[0].dateOfConnect").value("2015-01-20 15:30:45.0"))
-			    .andExpect(jsonPath("$[0].customerMasterT.customerId").value("CUS542"))
-			    .andExpect(jsonPath("$[0].customerMasterT.createdModifiedBy").value("287690"))
-			    .andExpect(jsonPath("$[0].customerMasterT.customerName").value("ABN Amro EU"))
-			    .andExpect(jsonPath("$[0].customerMasterT.documentsAttached").value("YES"))
-			    .andExpect(jsonPath("$[0].customerMasterT.groupCustomerName").value("ABN Amro"))
-			    .andDo(print())
-			    .andReturn();
-			List<ConnectT> connList=connectService.searchforConnectsByNameContaining("Cloud");
-				assertNotNull(connList);
-				assertEquals("2015-02-01 09:45:12.0", connList.get(0).getCreatedModifiedDatetime().toString());
-				assertEquals("2015-02-20 09:35:42.0", connList.get(0).getEndDatetimeOfConnect().toString());
-			}
-		
-		@Test
-		public void Test2ConnectByName() throws Exception
 		{
 			mockMvc.perform(get("/connect?nameWith=Cloud&fields=ConnectT,connectId,connectCategory,connectName").accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
@@ -167,11 +105,81 @@ public class ConnectControllerTest {
 				
 		
 		@Test
-		public void Test3ConnectByName() throws Exception
+		public void Test2ConnectByName() throws Exception
 		{
 			mockMvc.perform(get("/connect?nameWith=ABCD").accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isNotFound());
 }
+		
+		@Test
+		public void TestConnectByDateUserIdCustPartId() throws Exception
+		{
+			mockMvc1.perform(get("/connect/date?from=20012015&to=30012015&userId=541045&customerId=CUS541&partnerId=PAT4&fields=connectId,"
+					+ "connectCategory,connectName,createdModifiedBy,documentsAttached,primaryOwner,country")
+					.header("Authorization","Basic YWFhOmJiYg==")
+					.accept(MediaType.APPLICATION_JSON))
+					.andExpect(status().isOk())
+					.andExpect(content().contentType(TestUtil.APPLICATION_JSON_UTF8))
+					.andExpect(jsonPath("$[0].connectId").value("CNN2"))
+					.andExpect(jsonPath("$[0].connectCategory").value("PARTNER"))
+					.andExpect(jsonPath("$[0].connectName").value("DESS Capability Presentation"))
+					.andExpect(jsonPath("$[0].createdModifiedBy").value("198054"))
+					.andExpect(jsonPath("$[0].documentsAttached").value("YES"))
+					.andExpect(jsonPath("$[0].primaryOwner").value("541045"))
+					.andExpect(jsonPath("$[0].country").value("Netherlands"))
+					.andDo(print()).andReturn();
+		}
+		
+		@Test
+		public void TestConnectByDateCustId() throws Exception
+		{
+			mockMvc1.perform(get("/connect/date?from=01022015&to=20022015&customerId=CUS542&fields=connectId,"
+					+ "connectCategory,connectName,createdModifiedBy,documentsAttached,primaryOwner,partnerId,country")
+					.header("Authorization","Basic YWFhOmJiYg==")
+					.accept(MediaType.APPLICATION_JSON))
+					.andExpect(status().isOk())
+					.andExpect(content().contentType(TestUtil.APPLICATION_JSON_UTF8))
+					.andExpect(jsonPath("$[0].connectId").value("CNN3"))
+					.andExpect(jsonPath("$[0].connectCategory").value("CUSTOMER"))
+					.andExpect(jsonPath("$[0].connectName").value("Cloud Connect"))
+					.andExpect(jsonPath("$[0].createdModifiedBy").value("734628"))
+					.andExpect(jsonPath("$[0].documentsAttached").value("NO"))
+					.andExpect(jsonPath("$[0].primaryOwner").value("353911"))
+					.andExpect(jsonPath("$[0].country").value("USA"))
+					.andExpect(jsonPath("$[0].partnerId").value("PAT5"))
+					.andDo(print()).andReturn();
+		}
+		@Test
+		public void TestConnectByDatePartId() throws Exception
+		{
+			mockMvc1.perform(get("/connect/date?from=01022015&to=20022015&partnerId=PAT5&fields=connectId,connectCategory,"
+					+ "connectName,createdModifiedBy,documentsAttached,primaryOwner,customerId,partnerId,country")
+					.header("Authorization","Basic YWFhOmJiYg==")
+					.accept(MediaType.APPLICATION_JSON))
+					.andExpect(status().isOk())
+					.andExpect(content().contentType(TestUtil.APPLICATION_JSON_UTF8))
+					.andExpect(jsonPath("$[0].connectId").value("CNN3"))
+					.andExpect(jsonPath("$[0].connectCategory").value("CUSTOMER"))
+					.andExpect(jsonPath("$[0].connectName").value("Cloud Connect"))
+					.andExpect(jsonPath("$[0].createdModifiedBy").value("734628"))
+					.andExpect(jsonPath("$[0].documentsAttached").value("NO"))
+					.andExpect(jsonPath("$[0].primaryOwner").value("353911"))
+					.andExpect(jsonPath("$[0].country").value("USA"))
+					.andExpect(jsonPath("$[0].customerId").value("CUS542"))
+					.andExpect(jsonPath("$[0].partnerId").value("PAT5"))
+					
+					.andExpect(jsonPath("$[1].connectId").value("CNN6"))
+					.andExpect(jsonPath("$[1].connectCategory").value("PARTNER"))
+					.andExpect(jsonPath("$[1].connectName").value("DESS Capability  Presentation"))
+					.andExpect(jsonPath("$[1].createdModifiedBy").value("278648"))
+					.andExpect(jsonPath("$[1].documentsAttached").value("YES"))
+					.andExpect(jsonPath("$[1].primaryOwner").value("759726"))
+					.andExpect(jsonPath("$[1].country").value("USA"))
+					.andExpect(jsonPath("$[1].customerId").value("CUS545"))
+					.andExpect(jsonPath("$[1].partnerId").value("PAT5"))
+					.andDo(print()).andReturn();
+					
+		}
 		
 		@Test
 		public void Test1ConnectByDate() throws Exception
