44976fda55d9bf549df1023d9d920e7390763706
Muruganandam Nagalingam <muruganandam.nagalingam@tcs.com>
2015-04-16 17:16:06 +0530

    Opportunity Repository Update for My Target Dashboard service - Naresh

--- Destination/src/main/java/com/tcs/destination/data/repository/OpportunityRepository.java
+++ Destination/src/main/java/com/tcs/destination/data/repository/OpportunityRepository.java
@@ -34,7 +34,7 @@ public interface OpportunityRepository extends
 	@Query(value = "select sum(digital_deal_value) from opportunity_t where opportunity_owner=?1 and deal_closure_date >= ?2 and deal_closure_date <= ?3 and sales_stage_code =9", nativeQuery = true)
 	List<BigInteger> findDealValueForWins(String userId, Date fromDate,Date toDate);
 
-	@Query(value = "select * from opportunity_t where opportunity_owner=?1 and opportunity_id in (select opportunity_id from opportunity_timeline_history_t where  updated_datetime <= ?2 and sales_stage_code in (select max(sales_stage_code) from opportunity_timeline_history_t group by opportunity_id) and sales_stage_code between 3 and 8)", nativeQuery = true)
+	@Query(value = "select sum(digital_deal_value) from opportunity_t where opportunity_owner=?1 and opportunity_id in (select opportunity_id from opportunity_timeline_history_t where  updated_datetime <= ?2 and sales_stage_code in (select max(sales_stage_code) from opportunity_timeline_history_t group by opportunity_id) and sales_stage_code between 3 and 8)", nativeQuery = true)
 	List<BigInteger> findDealValueForPipeline(String userId,Timestamp endTime);
 
 }
