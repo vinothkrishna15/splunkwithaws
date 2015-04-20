21dcda296bac72aeac2ff259c3aaba735694ff52
Muruganandam Nagalingam <muruganandam.nagalingam@tcs.com>
2015-04-20 16:58:13 +0530

    WADL Generation - Badri

--- Destination/src/main/java/com/tcs/destination/DestinationApplication.java
+++ Destination/src/main/java/com/tcs/destination/DestinationApplication.java
@@ -12,7 +12,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @ImportResource("classpath:app-context.xml")
 @SpringBootApplication
 @EnableJpaRepositories
-@EnableWebMvc
 public class DestinationApplication extends SpringBootServletInitializer {
 
 	private static Class<DestinationApplication> applicationClass = DestinationApplication.class;
