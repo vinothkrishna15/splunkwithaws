be4c50b64b63550fb68c57b40c5028ffc49d76cc
Murugan <naresh.r1@tcs.com>
2015-05-27 16:35:44 +0530

    Contact Repository Query change in nameWith Service to accommodate latest table change - Naresh

--- Destination/src/main/java/com/tcs/destination/data/repository/ContactRepository.java
+++ Destination/src/main/java/com/tcs/destination/data/repository/ContactRepository.java
@@ -13,7 +13,7 @@ import com.tcs.destination.bean.ContactT;
  */
 public interface ContactRepository extends CrudRepository<ContactT, String> {
 
-	@Query(value = "select * from contact_t where UPPER(contact_name) like UPPER(?1) and (customer_id = ?2 or ?2='') and (partner_id = ?3 or ?3='') order by contact_name", nativeQuery = true)
+	@Query(value = "select distinct(CON.*) from contact_t CON,contact_customer_link_t CCLT where UPPER(CON.contact_name) like UPPER(?1) and ((CON.contact_id=CCLT.contact_id and CCLT.customer_id = ?2) or ?2='') and (CON.partner_id = ?3 or ?3='') order by contact_name", nativeQuery = true)
 	List<ContactT> findByContactName(String contactName, String customerId, String partnerId);
 
 	List<ContactT> findByContactNameIgnoreCaseStartingWithOrderByContactNameAsc(String startsWith);
