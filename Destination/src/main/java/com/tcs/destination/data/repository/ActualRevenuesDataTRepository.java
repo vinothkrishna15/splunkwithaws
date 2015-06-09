ccb414ae1ca0dc427a864040d8bacf1ae5dd7b9e
Muruganandam Nagalingam <muruganandam.nagalingam@tcs.com>
2015-06-09 17:10:39 +0530

    Target Vs Actual Change considering Geography in Query - Naresh

--- Destination/src/main/java/com/tcs/destination/data/repository/ActualRevenuesDataTRepository.java
+++ Destination/src/main/java/com/tcs/destination/data/repository/ActualRevenuesDataTRepository.java
@@ -2,7 +2,6 @@ package com.tcs.destination.data.repository;
 
 import java.util.List;
 
-
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -13,21 +12,16 @@ import com.tcs.destination.bean.ActualRevenuesDataT;
 public interface ActualRevenuesDataTRepository extends
 		CrudRepository<ActualRevenuesDataT, String> {
 
-	@Query(value="select ARDT.quarter, case when sum(ARDT.revenue) is not null then sum(ARDT.revenue) else '0.0' end as actual_revenue from actual_revenues_data_t ARDT "
+	@Query(value = "select ARDT.quarter, case when sum(ARDT.revenue) is not null then sum(ARDT.revenue) else '0.0' end as actual_revenue from actual_revenues_data_t ARDT "
 			+ "join geography_mapping_t GMT on ARDT.finance_geography = GMT.geography and (GMT.display_geography = ?3 or ?3 = '') "
 			+ "join iou_customer_mapping_t ICMT on ARDT.finance_iou = ICMT.iou and (ICMT.display_iou = ?4 or ?4 = '') "
 			+ "join sub_sp_mapping_t SSMT on ARDT.sub_sp = SSMT.actual_sub_sp and (SSMT.display_sub_sp = ?6 or ?6 = '') "
-			+ "join revenue_customer_mapping_t RCMT on ARDT.finance_customer_name = RCMT.finance_customer_name and "
+			+ "join revenue_customer_mapping_t RCMT on "
+			+ "(ARDT.finance_customer_name = RCMT.finance_customer_name and ARDT.finance_geography = RCMT.customer_geography) and "
 			+ "(RCMT.customer_name = ?5 or ?5= '')  where ARDT.financial_year = ?1 and (ARDT.quarter = ?2 or ?2 = '') "
-			+ "group by ARDT.quarter order by ARDT.quarter asc ", nativeQuery=true)
-	List<Object[]> findActualRevenue(
-			String financialYear,
-			String quarter,
-			String geography,
-			String iou,
-			String customerName,
+			+ "group by ARDT.quarter order by ARDT.quarter asc ", nativeQuery = true)
+	List<Object[]> findActualRevenue(String financialYear, String quarter,
+			String geography, String iou, String customerName,
 			String serviceLine);
-	
-	
 
 }
--- Destination/src/main/java/com/tcs/destination/data/repository/ProjectedRevenuesDataTRepository.java
+++ Destination/src/main/java/com/tcs/destination/data/repository/ProjectedRevenuesDataTRepository.java
@@ -13,21 +13,18 @@ import com.tcs.destination.bean.ProjectedRevenuesDataT;
 public interface ProjectedRevenuesDataTRepository extends
 		CrudRepository<ProjectedRevenuesDataT, String> {
 
-	@Query(value="select PRDT.quarter, case when sum(PRDT.revenue) is not null then sum(PRDT.revenue) else '0.0' end as projected_revenue from projected_revenues_data_t PRDT "
+	@Query(value = "select PRDT.quarter, case when sum(PRDT.revenue) is not null then sum(PRDT.revenue) else '0.0' end as projected_revenue from projected_revenues_data_t PRDT "
 			+ "join geography_mapping_t GMT on PRDT.finance_geography = GMT.geography and (GMT.display_geography = ?3 or ?3 = '') "
 			+ "join iou_customer_mapping_t ICMT on PRDT.finance_iou = ICMT.iou and (ICMT.display_iou = ?4 or ?4 = '') "
 			+ "join sub_sp_mapping_t SSMT on PRDT.sub_sp = SSMT.actual_sub_sp and (SSMT.display_sub_sp = ?6 or ?6 = '') "
-			+ "join revenue_customer_mapping_t RCMT on PRDT.finance_customer_name = RCMT.finance_customer_name and "
+			+ "join revenue_customer_mapping_t RCMT on "
+			+ "(PRDT.finance_customer_name = RCMT.finance_customer_name and PRDT.finance_geography=RCMT.customer_geography) and "
 			+ "(RCMT.customer_name = ?5 or ?5= '')  where PRDT.financial_year = ?1 and (PRDT.quarter = ?2 or ?2 = '') "
-			+ "group by PRDT.quarter order by PRDT.quarter asc ", nativeQuery=true)
-	List<Object[]> findProjectedRevenue(
-			String financialYear,
-			String quarter,
-			String geography,
-			String iou,
-			String customerName,
+			+ "group by PRDT.quarter order by PRDT.quarter asc ", nativeQuery = true)
+	List<Object[]> findProjectedRevenue(String financialYear, String quarter,
+			String geography, String iou, String customerName,
 			String serviceLine);
-	
+
 	@Query(value = "select distinct ICMT.display_iou as displayIOU, case when Result.actualRevenue is not null then Result.actualRevenue else '0.0' end as revenue"
 			+ " from iou_customer_mapping_t ICMT left outer join"
 			+ " (select ICMT.display_iou as displayIOU, sum(PRDT.revenue) as actualRevenue"
@@ -36,49 +33,54 @@ public interface ProjectedRevenuesDataTRepository extends
 			+ " join geography_mapping_t GMT on PRDT.finance_geography = GMT.geography and (GMT.display_geography = ?3 or ?3 = '')"
 			+ " join sub_sp_mapping_t SSMT on PRDT.sub_sp = SSMT.actual_sub_sp and (SSMT.display_sub_sp = ?4 or ?4 = '')"
 			+ " group by ICMT.display_iou"
-			+ " order by actualRevenue desc) Result on ICMT.display_iou = Result.displayIOU order by revenue desc", nativeQuery=true)
-	  public List<Object[]> getRevenuesByIOU(String financialYear, String quarter, String geography, String serviceLine);
-	  
-	  @Query(value="select distinct SSMT.display_sub_sp as displaySubSp, case when Result.actualRevenue is not null then Result.actualRevenue else '0.0' end as revenue"
-			  +" from sub_sp_mapping_t SSMT left outer join" 
-			  +" (select SSMT.display_sub_sp as displaySubSp, sum(PRDT.revenue) as actualRevenue" 
-			  +" from sub_sp_mapping_t SSMT join projected_revenues_data_t PRDT on SSMT.actual_sub_sp = PRDT.sub_sp"
-			  +" and PRDT.financial_year = ?1 and (PRDT.quarter = ?2 or ?2 = '')"
-			  +" join geography_mapping_t GMT on PRDT.finance_geography = GMT.geography and (GMT.display_geography = ?3 or ?3 = '')"
-			  +" join iou_customer_mapping_t ICMT on PRDT.finance_iou = ICMT.iou and (ICMT.display_iou = ?5 or ?5 = '')"
-			  +" join revenue_customer_mapping_t RCMT on PRDT.finance_customer_name = RCMT.finance_customer_name and "
-			  +" (RCMT.customer_name = ?4 or ?4 = '')"
-			  +" group by SSMT.display_sub_sp"
-			  +" order by actualRevenue desc) Result" 
-			  +" on SSMT.display_sub_sp = Result.displaySubSp order by revenue desc",nativeQuery=true)
-	  public List<Object[]> getRevenuesBySubSp(String financialYear,String quarter,String geography,String customerName,String iou);
-	  
-	  @Query(value="select distinct GMT.display_geography as displayGeography, case when Result.actualRevenue is not null then Result.actualRevenue else '0.0' end as revenue"
-			  + " from geography_mapping_t GMT left outer join"
-			  + " (select GMT.display_geography as displayGeography, sum(PRDT.revenue) as actualRevenue"
-			  + " from geography_mapping_t GMT join projected_revenues_data_t PRDT on GMT.geography = PRDT.finance_geography" 
-			  + " and PRDT.financial_year = ?1 and (PRDT.quarter = ?2 or ?2 = '')"
-			  + " join sub_sp_mapping_t SSMT on PRDT.sub_sp = SSMT.actual_sub_sp and (SSMT.display_sub_sp = ?4 or ?4 = '')"
-			  + " join iou_customer_mapping_t ICMT on PRDT.finance_iou = ICMT.iou and (ICMT.display_iou = ?5 or ?5 = '')"
-			  + " join revenue_customer_mapping_t RCMT on PRDT.finance_customer_name = RCMT.finance_customer_name and"
-			  + " (RCMT.customer_name = ?3 or ?3 = '')"
-			  + " group by GMT.display_geography"
-			  + " order by actualRevenue desc) Result"
-			  + " on GMT.display_geography = Result.displayGeography order by revenue desc",nativeQuery=true)
-	  public List<Object[]> getRevenuesByDispGeo(String financialYear,String quarter,String customer,String subSp,String iou);
-	  
-	  @Query(value="select GMT.geography, case when Result.actualRevenue is not null then Result.actualRevenue else '0.0' end as revenue"
-			  + " from geography_mapping_t GMT left outer join" 
-			  + " (select GMT.geography as displayGeography, sum(PRDT.revenue) as actualRevenue" 
-			  + " from geography_mapping_t GMT left outer join projected_revenues_data_t PRDT on GMT.geography = PRDT.finance_geography" 
-			  + " and (GMT.display_geography = ?6 or ?6 = '')"
-			  + " join sub_sp_mapping_t SSMT on PRDT.sub_sp = SSMT.actual_sub_sp and (SSMT.display_sub_sp = ?4 or ?4 = '')"
-			  + " join iou_customer_mapping_t ICMT on PRDT.finance_iou = ICMT.iou and (ICMT.display_iou = ?5 or ?5 = '')"
-			  + " join revenue_customer_mapping_t RCMT on PRDT.finance_customer_name = RCMT.finance_customer_name and (RCMT.customer_name = ?3 or ?3 = '')"
-			  + " where PRDT.financial_year = ?1 and (PRDT.quarter = ?2 or ?2 = '')"
-			  + " group by GMT.geography"
-			  + " order by actualRevenue desc) Result on GMT.geography = Result.displayGeography"
-			  + " where GMT.display_geography = ?6 order by revenue desc",nativeQuery=true)
-	  public List<Object[]> getRevenuesBySubGeo(String financialYear,String quarter,String customer,String subSp,String iou,String geography);
+			+ " order by actualRevenue desc) Result on ICMT.display_iou = Result.displayIOU order by revenue desc", nativeQuery = true)
+	public List<Object[]> getRevenuesByIOU(String financialYear,
+			String quarter, String geography, String serviceLine);
+
+	@Query(value = "select distinct SSMT.display_sub_sp as displaySubSp, case when Result.actualRevenue is not null then Result.actualRevenue else '0.0' end as revenue"
+			+ " from sub_sp_mapping_t SSMT left outer join"
+			+ " (select SSMT.display_sub_sp as displaySubSp, sum(PRDT.revenue) as actualRevenue"
+			+ " from sub_sp_mapping_t SSMT join projected_revenues_data_t PRDT on SSMT.actual_sub_sp = PRDT.sub_sp"
+			+ " and PRDT.financial_year = ?1 and (PRDT.quarter = ?2 or ?2 = '')"
+			+ " join geography_mapping_t GMT on PRDT.finance_geography = GMT.geography and (GMT.display_geography = ?3 or ?3 = '')"
+			+ " join iou_customer_mapping_t ICMT on PRDT.finance_iou = ICMT.iou and (ICMT.display_iou = ?5 or ?5 = '')"
+			+ " join revenue_customer_mapping_t RCMT on PRDT.finance_customer_name = RCMT.finance_customer_name and "
+			+ " (RCMT.customer_name = ?4 or ?4 = '')"
+			+ " group by SSMT.display_sub_sp"
+			+ " order by actualRevenue desc) Result"
+			+ " on SSMT.display_sub_sp = Result.displaySubSp order by revenue desc", nativeQuery = true)
+	public List<Object[]> getRevenuesBySubSp(String financialYear,
+			String quarter, String geography, String customerName, String iou);
+
+	@Query(value = "select distinct GMT.display_geography as displayGeography, case when Result.actualRevenue is not null then Result.actualRevenue else '0.0' end as revenue"
+			+ " from geography_mapping_t GMT left outer join"
+			+ " (select GMT.display_geography as displayGeography, sum(PRDT.revenue) as actualRevenue"
+			+ " from geography_mapping_t GMT join projected_revenues_data_t PRDT on GMT.geography = PRDT.finance_geography"
+			+ " and PRDT.financial_year = ?1 and (PRDT.quarter = ?2 or ?2 = '')"
+			+ " join sub_sp_mapping_t SSMT on PRDT.sub_sp = SSMT.actual_sub_sp and (SSMT.display_sub_sp = ?4 or ?4 = '')"
+			+ " join iou_customer_mapping_t ICMT on PRDT.finance_iou = ICMT.iou and (ICMT.display_iou = ?5 or ?5 = '')"
+			+ " join revenue_customer_mapping_t RCMT on PRDT.finance_customer_name = RCMT.finance_customer_name and"
+			+ " (RCMT.customer_name = ?3 or ?3 = '')"
+			+ " group by GMT.display_geography"
+			+ " order by actualRevenue desc) Result"
+			+ " on GMT.display_geography = Result.displayGeography order by revenue desc", nativeQuery = true)
+	public List<Object[]> getRevenuesByDispGeo(String financialYear,
+			String quarter, String customer, String subSp, String iou);
+
+	@Query(value = "select GMT.geography, case when Result.actualRevenue is not null then Result.actualRevenue else '0.0' end as revenue"
+			+ " from geography_mapping_t GMT left outer join"
+			+ " (select GMT.geography as displayGeography, sum(PRDT.revenue) as actualRevenue"
+			+ " from geography_mapping_t GMT left outer join projected_revenues_data_t PRDT on GMT.geography = PRDT.finance_geography"
+			+ " and (GMT.display_geography = ?6 or ?6 = '')"
+			+ " join sub_sp_mapping_t SSMT on PRDT.sub_sp = SSMT.actual_sub_sp and (SSMT.display_sub_sp = ?4 or ?4 = '')"
+			+ " join iou_customer_mapping_t ICMT on PRDT.finance_iou = ICMT.iou and (ICMT.display_iou = ?5 or ?5 = '')"
+			+ " join revenue_customer_mapping_t RCMT on PRDT.finance_customer_name = RCMT.finance_customer_name and (RCMT.customer_name = ?3 or ?3 = '')"
+			+ " where PRDT.financial_year = ?1 and (PRDT.quarter = ?2 or ?2 = '')"
+			+ " group by GMT.geography"
+			+ " order by actualRevenue desc) Result on GMT.geography = Result.displayGeography"
+			+ " where GMT.display_geography = ?6 order by revenue desc", nativeQuery = true)
+	public List<Object[]> getRevenuesBySubGeo(String financialYear,
+			String quarter, String customer, String subSp, String iou,
+			String geography);
 
 }
