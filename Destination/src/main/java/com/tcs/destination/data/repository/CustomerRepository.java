3f205b786bd44cae55bd9a792f9596a4495426f9
Muruganandam Nagalingam <muruganandam.nagalingam@tcs.com>
2015-06-11 17:23:50 +0530

    Update Query for Top revenue Customers - Naresh

--- Destination/src/main/java/com/tcs/destination/data/repository/CustomerRepository.java
+++ Destination/src/main/java/com/tcs/destination/data/repository/CustomerRepository.java
@@ -16,14 +16,16 @@ public interface CustomerRepository extends
 
 	List<CustomerMasterT> findByCustomerId(String customerid);
 
-	List<CustomerMasterT> findByCustomerNameIgnoreCaseContainingOrderByCustomerNameAsc(String name);
+	List<CustomerMasterT> findByCustomerNameIgnoreCaseContainingOrderByCustomerNameAsc(
+			String name);
 
-	List<CustomerMasterT> findByCustomerNameIgnoreCaseStartingWithOrderByCustomerNameAsc(String name);
+	List<CustomerMasterT> findByCustomerNameIgnoreCaseStartingWithOrderByCustomerNameAsc(
+			String name);
 
 	@Query(value = "select * from customer_Master_T c ORDER BY c.created_Modified_Datetime desc Limit ?1", nativeQuery = true)
 	List<CustomerMasterT> findRecent(int count);
 
-	@Query(value = "select * from customer_master_t where customer_name IN(select a.CUSTOMER_NAME from REVENUE_CUSTOMER_MAPPING_T a join (select FINANCE_CUSTOMER_NAME,SUM(REVENUE) AS rev from ACTUAL_REVENUES_DATA_T where FINANCIAL_YEAR=?2 Group By FINANCE_CUSTOMER_NAME  Order By rev Desc Limit ?1) b on b.FINANCE_CUSTOMER_NAME = a.FINANCE_CUSTOMER_NAME) ", nativeQuery = true)
+	@Query(value = "select CMT.* from customer_master_t CMT,(select RCMT.customer_name,sum(ART.revenue) from actual_revenues_data_t ART,revenue_customer_mapping_t RCMT where financial_year=?2 and ART.finance_customer_name=RCMT.finance_customer_name and ART.finance_geography=RCMT.customer_geography group by RCMT.customer_name order by sum desc limit ?1) as RV where RV.customer_name=CMT.customer_name order by RV.sum desc", nativeQuery = true)
 	List<CustomerMasterT> findTopRevenue(int count, String financialYear);
 
 	@Query(value = "select 	ART.quarter,sum(ART.revenue) as actual from ACTUAL_REVENUES_DATA_T ART,REVENUE_CUSTOMER_MAPPING_T RCM where RCM.customer_name =?1 and ART.financial_year = ?2 and ART.finance_customer_name = RCM.finance_customer_name group by 	ART.QUARTER", nativeQuery = true)
@@ -32,12 +34,15 @@ public interface CustomerRepository extends
 	@Query(value = "SELECT B.Quarter,B.target FROM BEACON_DATA_T B,BEACON_CUSTOMER_MAPPING_T CM WHERE  B.beacon_customer_name =CM.beacon_customer_name AND B.FINANCIAL_YEAR=?2  AND CM.customer_name=?1", nativeQuery = true)
 	List<Object[]> findTarget(String customerName, String financialYear);
 
-	List<CustomerMasterT> findByGroupCustomerNameIgnoreCaseContainingOrderByGroupCustomerNameAsc(String groupCustName);
+	List<CustomerMasterT> findByGroupCustomerNameIgnoreCaseContainingOrderByGroupCustomerNameAsc(
+			String groupCustName);
 
-//	@Query(value = "update customer_master_t set logo = ?1  where customer_id=?2", nativeQuery = true)
-//	void addImage(byte[] imageBytes, String id);
-	
-	@Query(value="select ART.quarter,sum(ART.revenue) as actual from ACTUAL_REVENUES_DATA_T ART,REVENUE_CUSTOMER_MAPPING_T RCM where ART.financial_year like (?1) "
+	// @Query(value =
+	// "update customer_master_t set logo = ?1  where customer_id=?2",
+	// nativeQuery = true)
+	// void addImage(byte[] imageBytes, String id);
+
+	@Query(value = "select ART.quarter,sum(ART.revenue) as actual from ACTUAL_REVENUES_DATA_T ART,REVENUE_CUSTOMER_MAPPING_T RCM where ART.financial_year like (?1) "
 			+ "and ART.finance_customer_name = RCM.finance_customer_name and (ART.QUARTER= (?2) or (?2)  = '') "
 			+ "and RCM.customer_name in (select CMT.customer_name from customer_master_t CMT "
 			+ "where ( CMT.geography in (select geography from geography_mapping_t where (display_geography= (?3) OR (?3) =''))) "
@@ -45,13 +50,8 @@ public interface CustomerRepository extends
 			+ "and CMT.customer_id in (select customer_id from opportunity_t OPP "
 			+ "where OPP.opportunity_id in (select opportunity_id from opportunity_sub_sp_link_t "
 			+ "where sub_sp in (select sub_sp from sub_sp_mapping_t where (display_sub_sp= (?4) OR (?4) ='')))) "
-			+ "and (CMT.customer_id = (?6) OR (?6) ='')) group by ART.QUARTER", nativeQuery=true)
-	List<Object[]> findActualRevenue(
-			String financialYear,
-			String quarter,
-			String geography,
-			String serviceLine, 
-			String iou,
-			String customerId);
+			+ "and (CMT.customer_id = (?6) OR (?6) ='')) group by ART.QUARTER", nativeQuery = true)
+	List<Object[]> findActualRevenue(String financialYear, String quarter,
+			String geography, String serviceLine, String iou, String customerId);
 
 }
