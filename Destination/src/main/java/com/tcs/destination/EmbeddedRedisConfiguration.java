09f28661e1291209a8ca31a0282372bd3647d232
Muruganandam Nagalingam <muruganandam.nagalingam@tcs.com>
2015-08-12 16:28:06 +0530

    Session Management - External Redis Server Changes

--- Destination/src/main/java/com/tcs/destination/EmbeddedRedisConfiguration.java
+++ Destination/src/main/java/com/tcs/destination/EmbeddedRedisConfiguration.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.annotation.PreDestroy;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
@@ -15,17 +16,28 @@ import redis.embedded.RedisServer;
 @Configuration
 @EnableRedisHttpSession
 public class EmbeddedRedisConfiguration {
-    private static RedisServer redisServer;
+//    private static RedisServer redisServer;
 
+	@Value("${spring.redis.host}")
+	private String redisHost;
+	
+	@Value("${spring.redis.port}")
+	private int redisPort;
+	
     @Bean
     public JedisConnectionFactory connectionFactory() throws IOException {
-        redisServer = new RedisServer(Protocol.DEFAULT_PORT);
-        redisServer.start();
-        return new JedisConnectionFactory();
+    	JedisConnectionFactory connection = new JedisConnectionFactory();
+        connection.setPort(redisPort);
+        connection.setHostName(redisHost);
+        return connection;
+
+//        redisServer = new RedisServer(Protocol.DEFAULT_PORT);
+//        redisServer.start();
+//        connection.setPassword("secret");
     }
 
-    @PreDestroy
-    public void destroy() {
-        redisServer.stop();
-    }
+//    @PreDestroy
+ //   public void destroy() {
+   //     redisServer.stop();
+    //}
 }
