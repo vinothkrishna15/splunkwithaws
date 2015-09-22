ff0fa05191ecfa80ed0a55cd0a3baaa38725703c
Muruganandam Nagalingam <muruganandam.nagalingam@tcs.com>
2015-09-22 11:26:01 +0530

    User Notifications - Follow Connects, Opportunities, Tasks changes

--- Destination/src/main/java/com/tcs/destination/service/FollowedService.java
+++ Destination/src/main/java/com/tcs/destination/service/FollowedService.java
@@ -2,16 +2,30 @@ package com.tcs.destination.service;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
 
+import com.tcs.destination.bean.UserT;
 import com.tcs.destination.bean.UserTaggedFollowedT;
+import com.tcs.destination.data.repository.ConnectRepository;
 import com.tcs.destination.data.repository.FollowedRepository;
+import com.tcs.destination.data.repository.NotificationsEventFieldsTRepository;
+import com.tcs.destination.data.repository.OpportunityRepository;
+import com.tcs.destination.data.repository.TaskRepository;
+import com.tcs.destination.data.repository.UserNotificationSettingsRepository;
+import com.tcs.destination.data.repository.UserNotificationsRepository;
+import com.tcs.destination.data.repository.UserRepository;
 import com.tcs.destination.enums.EntityType;
 import com.tcs.destination.exception.DestinationException;
+import com.tcs.destination.helper.FollowNotifications;
+import com.tcs.destination.helper.NotificationHelper;
 import com.tcs.destination.utils.DateUtils;
 
 @Service
@@ -22,6 +36,33 @@ public class FollowedService {
 
 	@Autowired
 	FollowedRepository followedRepository;
+	
+	@Autowired
+	NotificationsEventFieldsTRepository notificationEventFieldsTRepository;
+
+	@Autowired
+	UserNotificationsRepository userNotificationsTRepository;
+
+	@Autowired
+	UserNotificationSettingsRepository userNotificationSettingsRepo;
+
+	@Autowired
+	ThreadPoolTaskExecutor notificationsTaskExecutor;
+	
+	@Autowired
+	TaskRepository taskRepository;
+	
+	@Autowired
+	ConnectRepository connectRepository;
+	
+	@Autowired
+	OpportunityRepository opportunityRepository;
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	@PersistenceContext
+	private EntityManager entityManager;
 
 	public List<UserTaggedFollowedT> findFollowedFor(String userId,
 			String entityType) throws Exception {
@@ -111,7 +152,9 @@ public class FollowedService {
 			followed.setCreatedModifiedDatetime(DateUtils.getCurrentTimeStamp());
 			try {
 				logger.debug("Saving the UserTaggedFollowed");
-				return followedRepository.save(followed) != null;
+				UserTaggedFollowedT followDBObj = followedRepository.save(followed);
+                processNotification(followDBObj);				
+				return followDBObj != null;
 			} catch (Exception e) {
 				logger.error("BAD_REQUEST" + e.getMessage());
 				throw new DestinationException(HttpStatus.BAD_REQUEST,
@@ -121,6 +164,51 @@ public class FollowedService {
 		logger.error("BAD_REQUEST: Invalid Entity Type");
 		throw new DestinationException(HttpStatus.BAD_REQUEST,
 				"Invalid Entity Type");
+	}
+
+	// This method initializes the required fields(according to each entity) for the notifications executor and starts it
+	private void processNotification(UserTaggedFollowedT followDBObj) {
+		logger.debug("Calling processNotifications() method");
+
+		FollowNotifications followNotificationsHelper = new FollowNotifications();
+		String taskId = followDBObj.getTaskId();
+		String connectId = followDBObj.getConnectId();
+		String opportunityId = followDBObj.getOpportunityId();
+
+		String followUserId = followDBObj.getUserId();
+		String createdUserId = followDBObj.getCreatedModifiedBy();
+
+		UserT followUser = userRepository.findOne(followUserId);
+		UserT createdUser = userRepository.findOne(createdUserId);
+
+		followNotificationsHelper.setFollowUserId(followUserId);
+		followNotificationsHelper.setCreatedUserId(createdUserId);
+		followNotificationsHelper.setFollowUser(followUser);
+		followNotificationsHelper.setCreatedUser(createdUser);
+		followNotificationsHelper.setNotificationsEventFieldsTRepository(notificationEventFieldsTRepository);
+		followNotificationsHelper.setUserNotificationsTRepository(userNotificationsTRepository);
+		followNotificationsHelper.setUserNotificationSettingsRepo(userNotificationSettingsRepo);
+		followNotificationsHelper.setEntityManagerFactory(entityManager.getEntityManagerFactory());
+
+		if (taskId != null){
+			followNotificationsHelper.setEntityId(taskId);
+			followNotificationsHelper.setEntityType(EntityType.TASK.name());
+			followNotificationsHelper.setCrudRepository(taskRepository);
+			followNotificationsHelper.setEventId(2);
+			notificationsTaskExecutor.execute(followNotificationsHelper);
+		} else if (connectId != null){
+			followNotificationsHelper.setEntityId(connectId);
+			followNotificationsHelper.setEntityType(EntityType.CONNECT.name());
+			followNotificationsHelper.setCrudRepository(connectRepository);
+			followNotificationsHelper.setEventId(3);
+			notificationsTaskExecutor.execute(followNotificationsHelper);
+		} else if(opportunityId != null) {
+			followNotificationsHelper.setEntityId(opportunityId);
+			followNotificationsHelper.setEntityType(EntityType.OPPORTUNITY.name());
+			followNotificationsHelper.setCrudRepository(opportunityRepository);
+			followNotificationsHelper.setEventId(3);
+			notificationsTaskExecutor.execute(followNotificationsHelper);
+		}
 	}
 
 	public void unFollow(String userTaggedFollowedId) throws Exception {
