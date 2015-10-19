e10b77b23e80060384d0cb38a58252da6e716409
Muruganandam Nagalingam <muruganandam.nagalingam@tcs.com>
2015-10-19 20:25:22 +0530

    commented commentsTRepo- Achari

--- Destination/src/main/java/com/tcs/destination/data/repository/CommentsTRepository.java
+++ Destination/src/main/java/com/tcs/destination/data/repository/CommentsTRepository.java
@@ -3,12 +3,13 @@ package com.tcs.destination.data.repository;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.tcs.destination.bean.CommentsT;
 
-
+@Repository
 public interface CommentsTRepository extends CrudRepository<CommentsT, String> {
 
-	List<CommentsT> findByConnectId(String connectId);
+//	List<CommentsT> findByConnectId(String connectId);
 
 }
