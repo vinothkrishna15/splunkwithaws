c2d7e41763f70391642eff9f555dc40d94f6faef
Murugan <naresh.r1@tcs.com>
2015-11-21 20:38:19 +0530

    Opportunity Download - Shylaja

--- Destination/src/main/java/com/tcs/destination/data/repository/SalesStageMappingRepository.java
+++ Destination/src/main/java/com/tcs/destination/data/repository/SalesStageMappingRepository.java
@@ -1,5 +1,8 @@
 package com.tcs.destination.data.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +12,7 @@ import com.tcs.destination.bean.SalesStageMappingT;
 public interface SalesStageMappingRepository extends
 		CrudRepository<SalesStageMappingT, Integer> {
 	SalesStageMappingT findBySalesStageCode(Integer salesStageCode);
+	
+	@Query(value="select * from sales_stage_mapping_t",nativeQuery=true)
+	List<SalesStageMappingT> getSalesStageCodes();
 }
