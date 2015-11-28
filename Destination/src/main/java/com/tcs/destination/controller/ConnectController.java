a33a2f92ef7ccb12972028f762666c34a82633d9
Muruganandam Nagalingam <muruganandam.nagalingam@tcs.com>
2015-11-28 11:49:28 +0530

    Connect Controller : Handling Exception - Badri

--- Destination/src/main/java/com/tcs/destination/controller/ConnectController.java
+++ Destination/src/main/java/com/tcs/destination/controller/ConnectController.java
@@ -78,12 +78,22 @@ public class ConnectController {
 			@PathVariable("id") String connectId,
 			@RequestParam(value = "fields", defaultValue = "all") String fields,
 			@RequestParam(value = "view", defaultValue = "") String view)
-			throws Exception {
+			throws DestinationException {
 		logger.debug("Inside ConnectController /connect/id=" + connectId
 				+ " GET");
-		ConnectT connect = connectService.findConnectById(connectId);
-		return ResponseConstructors.filterJsonForFieldAndViews(fields, view,
-				connect);
+		ConnectT connect;
+		try {
+			connect = connectService.findConnectById(connectId);
+		
+			return ResponseConstructors.filterJsonForFieldAndViews(fields, view,
+					connect);
+		} catch (DestinationException e) {
+			throw e;
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+			throw new DestinationException(HttpStatus.INTERNAL_SERVER_ERROR,
+					"Backend error in retrieving connect details");
+		}
 	}
 
 	/**
@@ -103,14 +113,22 @@ public class ConnectController {
 			@RequestParam(value = "fields", defaultValue = "all") String fields,
 			@RequestParam(value = "customerId", defaultValue = "") String customerId,
 			@RequestParam(value = "view", defaultValue = "") String view)
-			throws Exception {
+			throws DestinationException {
 		logger.debug("Inside ConnectController /connect?nameWith="
 				+ connectName + " GET");
+		try{
 		PaginatedResponse paginatedConnect = connectService
 				.searchforConnectsByNameContaining(connectName, customerId,
 						page, count);
 		return ResponseConstructors.filterJsonForFieldAndViews(fields, view,
 				paginatedConnect);
+		} catch (DestinationException e) {
+			throw e;
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+			throw new DestinationException(HttpStatus.INTERNAL_SERVER_ERROR,
+					"Backend error in retrieving connects list");
+		}
 	}
 
 	/**
@@ -134,8 +152,8 @@ public class ConnectController {
 			@RequestParam(value = "weekEndDate", defaultValue = "01011970") @DateTimeFormat(pattern = "ddMMyyyy") Date weekEndDate,
 			@RequestParam(value = "monthStartDate", defaultValue = "01011970") @DateTimeFormat(pattern = "ddMMyyyy") Date monthStartDate,
 			@RequestParam(value = "monthEndDate", defaultValue = "01011970") @DateTimeFormat(pattern = "ddMMyyyy") Date monthEndDate)
-			throws Exception {
-		
+			throws DestinationException {
+		try{
 		String userId=DestinationUtils.getCurrentUserDetails().getUserId();
 		logger.debug("Inside ConnectController /connect/date?from=" + fromDate
 				+ "&to=" + toDate + "GET");
@@ -156,12 +174,19 @@ public class ConnectController {
 			return ResponseConstructors.filterJsonForFieldAndViews(fields,
 					view, dashBoardConnectsResponse);
 		}
+		} catch (DestinationException e) {
+			throw e;
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+			throw new DestinationException(HttpStatus.INTERNAL_SERVER_ERROR,
+					"Backend error in retrieving connects details");
+		}
 
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
 	public @ResponseBody ResponseEntity<String> insertToConnect(
-			@RequestBody ConnectT connect) throws Exception {
+			@RequestBody ConnectT connect) throws DestinationException {
 		logger.debug("Connect Insert Request Received /connect POST");
 		Status status = new Status();
 		status.setStatus(Status.FAILED, "");
@@ -170,19 +195,22 @@ public class ConnectController {
 				status.setStatus(Status.SUCCESS, connect.getConnectId());
 				logger.debug("CONNECT CREATED SUCCESS" + connect.getConnectId());
 			}
+			return new ResponseEntity<String>(
+					ResponseConstructors.filterJsonForFieldAndViews("all", "",
+							status), HttpStatus.OK);
+		} catch (DestinationException e) {
+			throw e;
 		} catch (Exception e) {
-			logger.error("INTERNAL_SERVER_ERROR" + e.getMessage());
+			logger.error(e.getMessage());
 			throw new DestinationException(HttpStatus.INTERNAL_SERVER_ERROR,
-					e.getMessage());
+					"Backend error while inserting connect");
 		}
-		return new ResponseEntity<String>(
-				ResponseConstructors.filterJsonForFieldAndViews("all", "",
-						status), HttpStatus.OK);
+		
 	}
 
 	@RequestMapping(method = RequestMethod.PUT)
 	public @ResponseBody ResponseEntity<String> editConnect(
-			@RequestBody ConnectT connect) throws Exception {
+			@RequestBody ConnectT connect) throws DestinationException {
 		logger.debug("Connect Edit Request Received /connect PUT");
 		Status status = new Status();
 		status.setStatus(Status.FAILED, "");
@@ -190,15 +218,18 @@ public class ConnectController {
 			if (connectService.updateConnect(connect)) {
 				status.setStatus(Status.SUCCESS, connect.getConnectId());
 			}
+			return new ResponseEntity<String>(
+					ResponseConstructors.filterJsonForFieldAndViews("all", "",
+							status), HttpStatus.OK);
+		} catch (DestinationException e) {
+			throw e;
 		} catch (Exception e) {
-			logger.error("INTERNAL_SERVER_ERROR" + e.getMessage());
+			logger.error(e.getMessage());
 			throw new DestinationException(HttpStatus.INTERNAL_SERVER_ERROR,
-					e.getMessage());
+					"Backend error while updating connect");
 		}
 
-		return new ResponseEntity<String>(
-				ResponseConstructors.filterJsonForFieldAndViews("all", "",
-						status), HttpStatus.OK);
+		
 	}
 
 	/**
@@ -230,10 +261,11 @@ public class ConnectController {
 			@RequestParam(value = "weekEndDate", defaultValue = "01011970") @DateTimeFormat(pattern = "ddMMyyyy") Date weekEndDate,
 			@RequestParam(value = "monthStartDate", defaultValue = "01011970") @DateTimeFormat(pattern = "ddMMyyyy") Date monthStartDate,
 			@RequestParam(value = "monthEndDate", defaultValue = "01011970") @DateTimeFormat(pattern = "ddMMyyyy") Date monthEndDate)
-			throws Exception {
+			throws DestinationException {
 
 		logger.debug("Inside ConnectController /connect/team?from=" + fromDate
 				+ "&to=" + toDate + "&supervisorId " + supervisorId + "GET");
+		try{
 		DashBoardConnectsResponse dashBoardConnectsResponse = null;
 		// Calling the service method
 		dashBoardConnectsResponse = connectService.getTeamConnects(
@@ -243,6 +275,13 @@ public class ConnectController {
 				ResponseConstructors.filterJsonForFieldAndViews(fields, view,
 						dashBoardConnectsResponse), HttpStatus.OK);
 
+		} catch (DestinationException e) {
+			throw e;
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+			throw new DestinationException(HttpStatus.INTERNAL_SERVER_ERROR,
+					"Backend error while retrieving Team connects details");
+		}
 	}
 
 	@RequestMapping(value = "/upload", method = RequestMethod.POST)
@@ -250,12 +289,13 @@ public class ConnectController {
 			@RequestParam("file") MultipartFile file,
 			@RequestParam(value = "fields", defaultValue = "all") String fields,
 			@RequestParam(value = "view", defaultValue = "") String view)
-			throws Exception {
-		String userId=DestinationUtils.getCurrentUserDetails().getUserId();
+			throws DestinationException {
+		
 		logger.debug("Upload request Received : docName ");
 		UploadStatusDTO status = null;
 		List<UploadServiceErrorDetailsDTO> errorDetailsDTOs = null;
 		try {
+			String userId=DestinationUtils.getCurrentUserDetails().getUserId();
 			status = connectUploadService.saveConnectDocument(file, userId);
 			if (status != null) {
 				System.out.println(status.isStatusFlag());
@@ -266,11 +306,7 @@ public class ConnectController {
 				}
 			}
 			logger.debug("UPLOAD SUCCESS - Record Created ");
-		} catch (Exception e) {
-			logger.error("INTERNAL_SERVER_ERROR" + e.getMessage());
-			throw new DestinationException(HttpStatus.INTERNAL_SERVER_ERROR,
-					e.getMessage());
-		}
+		
 		InputStreamResource excelFile = uploadErrorReport
 				.getErrorSheet(errorDetailsDTOs);
 		HttpHeaders respHeaders = new HttpHeaders();
@@ -281,6 +317,13 @@ public class ConnectController {
 				"upload_error.xlsx");
 		return new ResponseEntity<InputStreamResource>(excelFile, respHeaders,
 				HttpStatus.OK);
+		} catch (DestinationException e) {
+			throw e;
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+			throw new DestinationException(HttpStatus.INTERNAL_SERVER_ERROR,
+					"Backend error while uploading connects");
+		}
 	}
 
 	/**
@@ -298,24 +341,27 @@ public class ConnectController {
 			@RequestParam(value = "fields", defaultValue = "") String fields,
 			@RequestParam(value = "view", defaultValue = "") String view,
 			@RequestParam(value = "status", defaultValue = "ALL") String status,
-			@RequestParam("fy") String financialYear) throws Exception {
+			@RequestParam("fy") String financialYear) throws DestinationException {
 
 		List<ConnectT> listOfConnects = null;
-//		try {
+		try {
 			listOfConnects = connectService.getAllConnectsForDashbaord(status,
 					financialYear);
 			if (listOfConnects == null) {
 				logger.error("NOT_FOUND : No Connects found for the status {} and FY {}", status, financialYear);
 				throw new DestinationException(HttpStatus.NOT_FOUND, "No Connects found for the status " + status + " and FY " + financialYear);
 			}
-//		} catch (Exception e) {
-//			logger.error("INTERNAL_SERVER_ERROR" + e.getMessage());
-//			throw new DestinationException(HttpStatus.INTERNAL_SERVER_ERROR,
-//					e.getMessage());
-//		}
-		return new ResponseEntity<String>(
-				ResponseConstructors.filterJsonForFieldAndViews(fields, view,
-						listOfConnects), HttpStatus.OK);
+
+			return new ResponseEntity<String>(
+					ResponseConstructors.filterJsonForFieldAndViews(fields, view,
+							listOfConnects), HttpStatus.OK);
+		} catch (DestinationException e) {
+			throw e;
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+			throw new DestinationException(HttpStatus.INTERNAL_SERVER_ERROR,
+					"Backend error while retrieving connect details");
+		}
 	}
 
 	/**
@@ -335,21 +381,29 @@ public class ConnectController {
 			@RequestParam(value = "keyword", defaultValue = "") String keyword,
 			@RequestParam(value = "fields", defaultValue = "all") String fields,
 			@RequestParam(value = "view", defaultValue = "") String view)
-			throws Exception {
+			throws DestinationException {
 		logger.debug("Inside ConnectService /name GET");
 		List<ConnectNameKeywordSearch> searchResults = null;
 
-		searchResults = connectService.findConnectNameOrKeywords(name, keyword);
-		if ((searchResults == null) || (searchResults.isEmpty())) {
-			logger.error("No Results found for name {} and keyword {}", name,
-					keyword);
-			throw new DestinationException(HttpStatus.NOT_FOUND,
-					"No Results found for name " + name + " and keyword "
-							+ keyword);
-		}
+		try{
+			searchResults = connectService.findConnectNameOrKeywords(name, keyword);
+			if ((searchResults == null) || (searchResults.isEmpty())) {
+				logger.error("No Results found for name {} and keyword {}", name,
+						keyword);
+				throw new DestinationException(HttpStatus.NOT_FOUND,
+						"No Results found for name " + name + " and keyword "
+								+ keyword);
+			}
 
-		return ResponseConstructors.filterJsonForFieldAndViews(fields, view,
-				searchResults);
+			return ResponseConstructors.filterJsonForFieldAndViews(fields, view,
+					searchResults);
+		} catch (DestinationException e) {
+			throw e;
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+			throw new DestinationException(HttpStatus.INTERNAL_SERVER_ERROR,
+					"Backend error while retrieving connects list");
+		}
 	}
 
 	/**
@@ -360,9 +414,10 @@ public class ConnectController {
 	 */
 	@RequestMapping(value = "/download", method = RequestMethod.GET)
 	public @ResponseBody ResponseEntity<InputStreamResource> downloadConnect(
-			@RequestParam("downloadConnects") boolean oppFlag) throws Exception {
+			@RequestParam("downloadConnects") boolean oppFlag) throws DestinationException {
 		String userId=DestinationUtils.getCurrentUserDetails().getUserId();
 		logger.debug("Download request Received : docName ");
+		try{
 		InputStreamResource excelFile = connectDownloadService
 				.getConnects(userId,oppFlag);
 		HttpHeaders respHeaders = new HttpHeaders();
@@ -378,6 +433,13 @@ public class ConnectController {
 		logger.debug("Connect Downloaded Successfully ");
 		return new ResponseEntity<InputStreamResource>(excelFile, respHeaders,
 				HttpStatus.OK);
+		} catch (DestinationException e) {
+			throw e;
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+			throw new DestinationException(HttpStatus.INTERNAL_SERVER_ERROR,
+					"Backend error while downloading connect details");
+		}
 	}
 	
 	/**
@@ -393,10 +455,18 @@ public class ConnectController {
 			@RequestParam("ids") List<String> connectIds,
 			@RequestParam(value = "fields", defaultValue = "all") String fields,
 			@RequestParam(value = "view", defaultValue = "") String view)
-			throws Exception {
+			throws DestinationException {
 		logger.debug("Inside ConnectController /connect/id=" + connectIds + " GET");
-		List<ConnectT> connectList = connectService.getConnectsByConnetIds(connectIds);
-		return ResponseConstructors.filterJsonForFieldAndViews(fields, view, connectList);
+		try{
+			List<ConnectT> connectList = connectService.getConnectsByConnetIds(connectIds);
+			return ResponseConstructors.filterJsonForFieldAndViews(fields, view, connectList);
+		} catch (DestinationException e) {
+			throw e;
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+			throw new DestinationException(HttpStatus.INTERNAL_SERVER_ERROR,
+					"Backend error while retrieving connects list");
+		}
 	}
 
 }
