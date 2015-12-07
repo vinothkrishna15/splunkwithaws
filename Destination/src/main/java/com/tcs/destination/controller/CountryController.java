89788e3bfd988911b90c625b6a3535b3805979a0
Murugan <naresh.r1@tcs.com>
2015-12-07 16:14:09 +0530

    Added logging and Exception changes in Country,Currency,Customer and Dashboard Controller - Mani

--- Destination/src/main/java/com/tcs/destination/controller/CountryController.java
+++ Destination/src/main/java/com/tcs/destination/controller/CountryController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tcs.destination.bean.GeographyCountryMappingT;
+import com.tcs.destination.exception.DestinationException;
 import com.tcs.destination.service.CountryService;
 import com.tcs.destination.utils.ResponseConstructors;
 
@@ -27,13 +29,23 @@ public class CountryController {
 	@RequestMapping(method = RequestMethod.GET)
 	public @ResponseBody String findAll(
 	@RequestParam(value = "fields", defaultValue = "all") String fields,
-	@RequestParam(value = "view", defaultValue = "") String view) throws Exception
+	@RequestParam(value = "view", defaultValue = "") String view) throws DestinationException
 	{
 		logger.debug("Inside CountryController /country GET");
+		logger.info("Start of retrieving the Geography Country Mapping");
 		ArrayList<GeographyCountryMappingT> geographyCountryMapping=new ArrayList<GeographyCountryMappingT>();
+		try {
 		geographyCountryMapping=(ArrayList<GeographyCountryMappingT>) countryService.findAll();
+		logger.info("End of retrieving the Geography Country Mapping");
 		return ResponseConstructors.filterJsonForFieldAndViews(fields, view,
 				geographyCountryMapping);
+		} catch (DestinationException e) {
+			throw e;
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+			throw new DestinationException(HttpStatus.INTERNAL_SERVER_ERROR,
+					"Backend error in retrieving the Geography Country Mapping");
+	   }
 	}
 
 }
