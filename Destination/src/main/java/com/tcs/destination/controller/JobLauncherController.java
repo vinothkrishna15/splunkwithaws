b8397a8de931e2e9b996cc39a0cee15fc2a9d771
Muruganandam Nagalingam <muruganandam.nagalingam@tcs.com>
2015-12-09 19:16:45 +0530

    Improving exception handling and logging in job launcher - Rajiniganth J

--- Destination/src/main/java/com/tcs/destination/controller/JobLauncherController.java
+++ Destination/src/main/java/com/tcs/destination/controller/JobLauncherController.java
@@ -8,8 +8,13 @@ import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameter;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.configuration.JobRegistry;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.NoSuchJobException;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -51,30 +56,53 @@ public class JobLauncherController {
     public @ResponseBody ResponseEntity<String> jobLaunch(
 	    @RequestBody  JobName jobName,
 	    @RequestParam(value = "fields", defaultValue = "all") String fields,
-	    @RequestParam(value = "view", defaultValue = "") String view)
-	    throws Exception {
+	    @RequestParam(value = "view", defaultValue = "") String view) throws Exception
+	     {
 		
-		logger.debug("Launching job");
+		logger.info("Launching job:" + jobName.getJob());
 		
 		Status status = new Status();
 		status.setStatus(Status.FAILED, "");
-		try {
-			Job job = jobRegistry.getJob(jobName.getJob());
-			JobExecution execution = jobLauncher.run(job, getJobParameter(job));
-			
-			logger.info("Job: {} exit status:{}.", job.getName() ,execution.getStatus() );
-			
-			if (execution.getStatus().equals(BatchStatus.COMPLETED)) {
-				status.setStatus(Status.SUCCESS, "Job:" + job + " completed successfully.");
+			Job job;
+			JobExecution execution;
+			try {
+				job = jobRegistry.getJob(jobName.getJob());
+				execution = jobLauncher.run(job, getJobParameter(job));
+				
+				logger.info("Job: {} exit status:{}.", job.getName() ,execution.getStatus() );
+				
+				if (execution.getStatus().equals(BatchStatus.COMPLETED)) {
+					status.setStatus(Status.SUCCESS, "Job:" + job + " completed successfully.");
+				}
+				
+			} catch (NoSuchJobException e) {
+				logger.error("INTERNAL_SERVER_ERROR" + e.getMessage());
+			    throw new DestinationException(HttpStatus.INTERNAL_SERVER_ERROR,
+				    "No such job available for the job name:" + jobName.getJob());
+			} catch (JobExecutionAlreadyRunningException e) {
+				logger.error("INTERNAL_SERVER_ERROR" + e.getMessage());
+			    throw new DestinationException(HttpStatus.INTERNAL_SERVER_ERROR,
+				    "Error lauching the job as an job instance is already running:");
+			} catch (JobRestartException e) {
+				logger.error("INTERNAL_SERVER_ERROR" + e.getMessage());
+			    throw new DestinationException(HttpStatus.INTERNAL_SERVER_ERROR,
+				    "Unable to restart the job:" + jobName.getJob());
+			} catch (JobInstanceAlreadyCompleteException e) {
+				logger.error("INTERNAL_SERVER_ERROR" + e.getMessage());
+			    throw new DestinationException(HttpStatus.INTERNAL_SERVER_ERROR,
+				    "Error lauching the job as a job instance is already completed:");
+			} catch (JobParametersInvalidException e) {
+				logger.error("INTERNAL_SERVER_ERROR" + e.getMessage());
+			    throw new DestinationException(HttpStatus.INTERNAL_SERVER_ERROR,
+				    "Error lauching the job due to invalid job parameters:" + jobName.getJob());
+			} catch (Exception e) {
+				 logger.error("INTERNAL_SERVER_ERROR" + e.getMessage());
+				    throw new DestinationException(HttpStatus.INTERNAL_SERVER_ERROR,
+					    "Error lauching the job:" + jobName.getJob());
 			}
-		} catch (DestinationException e) {
-		    logger.error("Destination Exception" + e.getMessage());
-		    throw e;
-		} catch (Exception e) {
-		    logger.error("INTERNAL_SERVER_ERROR" + e.getMessage());
-		    throw new DestinationException(HttpStatus.INTERNAL_SERVER_ERROR,
-			    e.getMessage());
-		}
+			
+			
+		
 		return new ResponseEntity<String>(
 				ResponseConstructors.filterJsonForFieldAndViews(fields, view,
 						status), HttpStatus.OK);
