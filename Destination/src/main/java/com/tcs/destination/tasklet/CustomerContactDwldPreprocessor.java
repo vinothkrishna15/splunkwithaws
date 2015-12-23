4968a12929362d22bc8a1b7cb9ecb4ad2d0a96ae
Renukadevi Chandramouli <renukadevi.chandramouli@tcs.com>
2015-12-23 17:13:01 +0530

    Customer Contact Download Batch

---
+++ Destination/src/main/java/com/tcs/destination/tasklet/CustomerContactDwldPreprocessor.java
@@ -0,0 +1,89 @@
+package com.tcs.destination.tasklet;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import static com.tcs.destination.enums.JobStep.CUSTOMER_CONTACT_DWLD_PROCESSING;
+import static com.tcs.destination.enums.JobStep.END;
+import static com.tcs.destination.enums.RequestType.CUSTOMER_CONTACT_DOWNLOAD;
+import static com.tcs.destination.enums.RequestStatus.SUBMITTED;
+import static com.tcs.destination.utils.Constants.NEXT_STEP;
+import static com.tcs.destination.utils.Constants.REQUEST;
+
+import com.tcs.destination.bean.ContactCustomerLinkT;
+import com.tcs.destination.bean.CustomerMasterT;
+import com.tcs.destination.bean.DataProcessingRequestT;
+import com.tcs.destination.data.repository.ContactCustomerLinkTRepository;
+import com.tcs.destination.data.repository.DataProcessingRequestRepository;
+
+@Component("customerContactDwldPreprocessor")
+public class CustomerContactDwldPreprocessor implements Tasklet {
+	private static final Logger logger = LoggerFactory
+			.getLogger(CustomerContactDwldPreprocessor.class);
+
+	private List<DataProcessingRequestT> requestList = null;
+	
+	@Autowired
+	private ContactCustomerLinkTRepository contactCustomerLinkTRepository;
+
+	@Autowired
+	private DataProcessingRequestRepository dataProcessingRequestRepository;
+	
+	private Map<String, CustomerMasterT> contactCustomerMap;
+
+	@Override
+	public RepeatStatus execute(StepContribution contribution,
+			ChunkContext chunkContext) throws Exception {
+		logger.debug("Inside customer contact download preprocessor");
+		
+		if (requestList == null) {
+			
+			requestList = dataProcessingRequestRepository
+					.findByRequestTypeAndStatus(
+							CUSTOMER_CONTACT_DOWNLOAD.getType(),
+							SUBMITTED.getStatus());
+		}
+			ExecutionContext jobContext = chunkContext.getStepContext()
+					.getStepExecution().getJobExecution().getExecutionContext();
+			
+			if (CollectionUtils.isNotEmpty(requestList)) {
+
+				getContactCustomerLinkT();
+				jobContext.put("mapOfContactCustomerLinkT",contactCustomerMap );
+				
+				DataProcessingRequestT request = requestList.remove(0);
+
+				jobContext.put(REQUEST, request);
+				jobContext.put(NEXT_STEP, CUSTOMER_CONTACT_DWLD_PROCESSING);
+
+			} else {
+				jobContext.put(NEXT_STEP, END);
+				requestList = null;
+			}			
+		
+		return RepeatStatus.FINISHED;
+	}
+	
+	private Map<String, CustomerMasterT> getContactCustomerLinkT() {
+		List<ContactCustomerLinkT> listOfContactCustomerLinkT = null;
+		listOfContactCustomerLinkT = (List<ContactCustomerLinkT>) contactCustomerLinkTRepository.findAll();
+		contactCustomerMap = new HashMap<String, CustomerMasterT>();
+		for (ContactCustomerLinkT contactCustomerMappingT : listOfContactCustomerLinkT) {
+			contactCustomerMap.put(contactCustomerMappingT.getContactId(), contactCustomerMappingT.getCustomerMasterT());
+		}
+		return contactCustomerMap;
+	}
+
+}
