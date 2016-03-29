f695e54978f0c2e69d111d220449d401ffb428f7
Mangala krishnaveni <mangala.krishanveni@tcs.com>
2016-03-29 14:14:23 +0530

    error message description

--- Destination/src/main/java/com/tcs/destination/controller/WorkflowController.java
+++ Destination/src/main/java/com/tcs/destination/controller/WorkflowController.java
@@ -151,7 +151,7 @@ public class WorkflowController {
 		try {
 			if (workflowService.rejectWorkflowEntity(workflowStepT)) {
 				status.setStatus(Status.SUCCESS, "Request " + workflowStepT.getRequestId() + " is REJECTED by " + userT.getUserName() + "!!!");
-				logger.debug("Request " + workflowStepT.getRequestId() + " is REJECTED by " + userT.getUserName() + "!!!");
+				logger.debug("Request is REJECTED by " + userT.getUserName() + "!!!");
 			}
 			logger.info("Inside WorkflowController: End of reject Customer");
 			return new ResponseEntity<String>(
