cbea5f1cf1b90e7cede0564ba1e159c5c80a71a8
Rajiniganth  Jagaeesan <rajiniganth.jagadeesan@tcs.com>
2016-05-10 18:42:25 +0530

    Number scale

--- Destination/src/main/java/com/tcs/destination/service/NumericUtil.java
+++ Destination/src/main/java/com/tcs/destination/service/NumericUtil.java
@@ -10,8 +10,11 @@
 package com.tcs.destination.service;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 import java.text.DecimalFormat;
 
+import static com.tcs.destination.utils.Constants.USD;
 import static com.tcs.destination.utils.Constants.USD_PATTERN;
 
 /**
@@ -20,7 +23,16 @@ import static com.tcs.destination.utils.Constants.USD_PATTERN;
  */
 public class NumericUtil {
 	
+	private static BigDecimal THOUSAND = new BigDecimal(1000);
+	private static BigDecimal MILLION = new BigDecimal(1000000);
+	private static BigDecimal BILLION = new BigDecimal(1000000000);
+	private static BigDecimal TRILLION = new BigDecimal(1000000000000L);
+	private static BigDecimal QUADRILLION = new BigDecimal(1000000000000000L);
+	public static MathContext mc = new MathContext(3, RoundingMode.HALF_EVEN);
+	
 	private static DecimalFormat usdFormatter = new DecimalFormat(USD_PATTERN);
+	
+	
 	
 	/**
 	 * Method to format it to the USD String
@@ -31,6 +43,25 @@ public class NumericUtil {
 	public static String toUSD(BigDecimal value) {
 		
 		return usdFormatter.format(value);
+	}
+	
+	public static String toUSDinNumberScale(BigDecimal value) {
+		
+		String returnVal = null;
+		
+		if (value.compareTo(THOUSAND) != 1) {
+			returnVal = ((Double)value.round(mc).doubleValue()).toString() + " " + USD;
+		} else if (value.compareTo(THOUSAND) != -1 && value.compareTo(MILLION) == -1) {
+			returnVal = ((Double)value.divide(THOUSAND, mc).round(mc).doubleValue()).toString() + "K " + USD;
+		} else if (value.compareTo(MILLION) != -1 && value.compareTo(BILLION) == -1) {
+			returnVal = ((Double)value.divide(MILLION, mc).round(mc).doubleValue()).toString() + "M " + USD;
+		} else if (value.compareTo(MILLION) != -1 && value.compareTo(TRILLION) == -1) {
+			returnVal = ((Double)value.divide(BILLION, mc).round(mc).doubleValue()).toString() + "B " + USD;
+		} else if (value.compareTo(TRILLION) != -1 && value.compareTo(QUADRILLION) == -1) {
+			returnVal = ((Double)value.divide(TRILLION, mc).round(mc).doubleValue()).toString() + "T " + USD;
+		}
+		
+		return returnVal;
 	}
 
 }
