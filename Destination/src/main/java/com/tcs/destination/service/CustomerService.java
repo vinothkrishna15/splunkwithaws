e1951f783813d6ea2a7fa9bdf96e3fe5ae3f25b9
Badri Naraayanan M <badrinaraayanan.m@tcs.com>
2016-05-11 16:56:45 +0530

    Customer - inactive & update

--- Destination/src/main/java/com/tcs/destination/service/CustomerService.java
+++ Destination/src/main/java/com/tcs/destination/service/CustomerService.java
@@ -1273,4 +1273,13 @@ public class CustomerService {
 		}
 		return oldCustomerObj;
 	}
+	/*
+	 * soft delete - status updated as inactive for the given customer list
+	 */
+	public void makeInactive(List<CustomerMasterT> deleteList) {
+		for(CustomerMasterT customer : deleteList){
+			customer.setActive(false);
+			customerRepository.save(customer);
+		}
+	}
 }
