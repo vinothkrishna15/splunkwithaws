4cfb384a799422a1aec2b7be1cddfe13c1219e9d
Mangala krishnaveni <mangala.krishanveni@tcs.com>
2016-05-27 13:03:10 +0530

    month reporting for deal closure

---
+++ Destination/src/main/java/com/tcs/destination/data/repository/DealReportingRepository.java
@@ -0,0 +1,12 @@
+package com.tcs.destination.data.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tcs.destination.bean.DealClosureReportingT;
+
+@Repository
+public interface DealReportingRepository extends CrudRepository<DealClosureReportingT, Integer>{
+	
+
+}
