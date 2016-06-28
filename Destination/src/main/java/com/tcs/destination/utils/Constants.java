11daaa9dff3ac1b07af12e945282b8411c2f301d
Mangala krishnaveni <mangala.krishanveni@tcs.com>
2016-06-28 17:55:05 +0530

    constants file

--- Destination/src/main/java/com/tcs/destination/utils/Constants.java
+++ Destination/src/main/java/com/tcs/destination/utils/Constants.java
@@ -219,6 +219,7 @@ public class Constants {
 	public static final int CONSTANT_ZERO = 0;
 	public static final int CONSTANT_ONE = 1;
 	public static final int CONSTANT_TWO = 2;
+	public static final int CONSTANT_NINE = 9;
 	
 	//For Opportunity win/loss mail
 	public static final String 	WITH_SUPPORT_FROM = "with support from";
