170e2460f71b2ba1de9e8e1a51958eaefe01dd77
Mangala krishnaveni <mangala.krishanveni@tcs.com>
2016-06-29 12:19:12 +0530

    competitor

--- Destination/src/main/java/com/tcs/destination/service/WorkflowService.java
+++ Destination/src/main/java/com/tcs/destination/service/WorkflowService.java
@@ -1410,7 +1410,7 @@ public class WorkflowService {
 			// Contains all the lists of partner requests
 			List<List<Object[]>> listOfPartnerRequests = new ArrayList<>();
 			// Contains all the lists of partner requests
-			//List<List<Object[]>> listOfCompetitorRequests = new ArrayList<>();
+			List<List<Object[]>> listOfCompetitorRequests = new ArrayList<>();
 			// Contains all the lists of partner requests
 			List<List<Object[]>> listOfOpportunityReopenRequests = new ArrayList<>();
 
@@ -1426,7 +1426,7 @@ public class WorkflowService {
 				List<Object[]> pendingCustomerRequests = getPendingCustomerRequests(userId);
 				List<Object[]> pendingPartnerRequests = getPendingPartnerRequests(userId);
 
-			//	List<Object[]> pendingCompetitorRequests = getPendingCompetitorRequests(userId);
+				List<Object[]> pendingCompetitorRequests = getPendingCompetitorRequests(userId);
 				List<Object[]> pendingOpportunityReopenRequests = getPendingOpportunityReopenRequests(userId);
 
 				// Add all the lists of customer requests
@@ -1436,7 +1436,7 @@ public class WorkflowService {
 				listOfPartnerRequests.add(pendingPartnerRequests);
 
 				// Add all the lists of competitor requests
-				//listOfCompetitorRequests.add(pendingCompetitorRequests);
+				listOfCompetitorRequests.add(pendingCompetitorRequests);
 
 				// Add all the lists of opportunity re-open requests
 				listOfOpportunityReopenRequests
@@ -1448,8 +1448,8 @@ public class WorkflowService {
 					EntityType.CUSTOMER.toString(), myWorklist);
 			populateResponseList(listOfPartnerRequests,
 					EntityType.PARTNER.toString(), myWorklist);
-//			populateResponseList(listOfCompetitorRequests,
-//					EntityType.COMPETITOR.toString(), myWorklist);
+			populateResponseList(listOfCompetitorRequests,
+					EntityType.COMPETITOR.toString(), myWorklist);
 			populateResponseList(listOfOpportunityReopenRequests,
 					EntityType.OPPORTUNITY.toString(), myWorklist);
 
@@ -1615,12 +1615,12 @@ public class WorkflowService {
 							myWorklistDTO.setEntityName(workflowPartnerRepository
 									.findOne(requestT.getEntityId()).getPartnerName());
 							break;
-//						case COMPETITOR:
-//							myWorklistDTO.setEntityType(COMPETITOR.getDisplayName());
-//							myWorklistDTO.setEntityName(workflowCompetitorRepository
-//									.findOne(requestT.getEntityId())
-//									.getWorkflowCompetitorName());
-//							break;
+						case COMPETITOR:
+							myWorklistDTO.setEntityType(COMPETITOR.getDisplayName());
+							myWorklistDTO.setEntityName(workflowCompetitorRepository
+									.findOne(requestT.getEntityId())
+									.getWorkflowCompetitorName());
+							break;
 						case OPPORTUNITY:
 							myWorklistDTO.setEntityType(EntityTypeId.OPPORTUNITY
 									.getDisplayName());
