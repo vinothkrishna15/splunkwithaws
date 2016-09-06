6d746aa874b8c4fb0bd09aff48eecd9f0c0d175a
Badri Naraayanan M <badrinaraayanan.m@tcs.com>
2016-09-06 12:25:33 +0530

    Documents : Bean, controller, service, repository - Initial template classes

---
+++ Destination/src/main/java/com/tcs/destination/controller/DocumentsController.java
@@ -0,0 +1,21 @@
+package com.tcs.destination.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * This controller deals with the document related functionalities
+ * 
+ * @author TCS
+ *
+ */
+@RestController
+@RequestMapping("/doc")
+public class DocumentsController {
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(DocumentsController.class);
+
+}
---
+++ Destination/src/main/java/com/tcs/destination/service/DocumentsService.java
@@ -0,0 +1,19 @@
+package com.tcs.destination.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+/**
+ * This service is used to save and delete documents
+ * 
+ * @author TCS
+ *
+ */
+@Service
+public class DocumentsService {
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(DocumentsService.class);
+
+}
