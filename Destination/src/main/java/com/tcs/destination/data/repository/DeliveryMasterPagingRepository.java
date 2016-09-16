bbe84baa2a9eed36dc564e7c8f371e75e13a0b1f
Badri Naraayanan M <badrinaraayanan.m@tcs.com>
2016-09-16 18:50:35 +0530

    delivery list changes - commented codes removed

--- Destination/src/main/java/com/tcs/destination/data/repository/DeliveryMasterPagingRepository.java
+++ Destination/src/main/java/com/tcs/destination/data/repository/DeliveryMasterPagingRepository.java
@@ -11,60 +11,7 @@ import com.tcs.destination.bean.DeliveryMasterT;
 
 @Repository
 public interface DeliveryMasterPagingRepository extends PagingAndSortingRepository<DeliveryMasterT, Integer> {
-
-	
-	
 	Page<DeliveryMasterT> findByDeliveryManagerIdAndDeliveryStageIn(String userId,List<Integer>deliveryStages,Pageable pageable);
 	Page<DeliveryMasterT> findByDeliveryCentreIdAndDeliveryStageIn(Integer deliveryCentreId,List<Integer> deliveryStages,Pageable pageable);
 	Page<DeliveryMasterT> findByDeliveryCentreIdInAndDeliveryStageIn(List<Integer> deliveryCentreIds,List<Integer> deliveryStages,Pageable pageable);
-	//Page<DeliveryMasterT> findByUserIdAndEntityTypeIgnoreCaseOrderByCreatedDatetimeDesc(String userId, String entityType, Pageable pageable);
-	
-	
-	//for manager
-	//order by deliveryMasterId
-//	Page<DeliveryMasterT> findByDeliveryManagerIdAndDeliveryStageInOrderByDeliveryMasterId (String deliveryManagerId, List<Integer> deliveryStages,Pageable pageable);
-//	Page<DeliveryMasterT> findByDeliveryManagerIdAndDeliveryStageInOrderByDeliveryMasterIdDesc(String deliveryManagerId, List<Integer> deliveryStages, Pageable pageable);
-//	
-//	//order by opportunityId
-//	Page<DeliveryMasterT> findByDeliveryManagerIdAndDeliveryStageInOrderByOpportunityId (String deliveryManagerId,List<Integer> deliveryStages, Pageable pageable);
-//	Page<DeliveryMasterT> findByDeliveryManagerIdAndDeliveryStageInOrderByOpportunityIdDesc(String deliveryManagerId,List<Integer> deliveryStages, Pageable pageable);
-//	
-//	//order by deliveryCentreId
-//	Page<DeliveryMasterT> findByDeliveryManagerIdAndDeliveryStageInOrderByDeliveryCentreId (String deliveryManagerId,List<Integer> deliveryStages, Pageable pageable);
-//	Page<DeliveryMasterT> findByDeliveryManagerIdAndDeliveryStageInOrderByDeliveryCentreIdDesc(String deliveryManagerId,List<Integer> deliveryStages, Pageable pageable);
-//	
-//	
-//	
-//	
-//	//for centre head
-//	//order by deliveryMasterId
-//	Page<DeliveryMasterT> findByDeliveryCentreIdAndDeliveryStageInOrderByDeliveryMasterId(Integer deliveryCentreId,List<Integer> deliveryStages,Pageable pageable);
-//	Page<DeliveryMasterT> findByDeliveryCentreIdAndDeliveryStageInOrderByDeliveryMasterIdDesc(Integer deliveryCentreId,List<Integer> deliveryStages,Pageable pageable);
-//	
-//	//order by opportunityId
-//	Page<DeliveryMasterT> findByDeliveryCentreIdAndDeliveryStageInOrderByOpportunityId(Integer deliveryCentreId,List<Integer> deliveryStages,Pageable pageable);
-//	Page<DeliveryMasterT> findByDeliveryCentreIdAndDeliveryStageInOrderByOpportunityIdDesc(Integer deliveryCentreId,List<Integer> deliveryStages,Pageable pageable);
-//	
-//	//order by deliveryCentreId
-//	Page<DeliveryMasterT> findByDeliveryCentreIdAndDeliveryStageInOrderByDeliveryCentreId(Integer deliveryCentreId,List<Integer> deliveryStages,Pageable pageable);
-//	Page<DeliveryMasterT> findByDeliveryCentreIdAndDeliveryStageInOrderByDeliveryCentreIdDesc(Integer deliveryCentreId,List<Integer> deliveryStages,Pageable pageable);
-//
-//	
-//	
-//	
-//	//for cluster head
-//	//order by deliveryMasterId
-//	Page<DeliveryMasterT> findByDeliveryCentreIdInAndDeliveryStageInOrderByDeliveryMasterId(List<Integer> deliveryCentreIds,List<Integer> deliveryStages,Pageable pageable);
-//	Page<DeliveryMasterT> findByDeliveryCentreIdInAndDeliveryStageInOrderByDeliveryMasterIdDesc(List<Integer> deliveryCentreIds,List<Integer> deliveryStages,Pageable pageable);
-//		
-//	//order by opportunityId
-//	Page<DeliveryMasterT> findByDeliveryCentreIdInAndDeliveryStageInOrderByOpportunityId(List<Integer> deliveryCentreIds,List<Integer> deliveryStages,Pageable pageable);
-//	Page<DeliveryMasterT> findByDeliveryCentreIdInAndDeliveryStageInOrderByOpportunityIdDesc(List<Integer> deliveryCentreIds,List<Integer> deliveryStages,Pageable pageable);
-//		
-//	//order by deliveryCentreId
-//	Page<DeliveryMasterT> findByDeliveryCentreIdInAndDeliveryStageInOrderByDeliveryCentreId(List<Integer> deliveryCentreIds,List<Integer> deliveryStages,Pageable pageable);
-//	Page<DeliveryMasterT> findByDeliveryCentreIdInAndDeliveryStageInOrderByDeliveryCentreIdDesc(List<Integer> deliveryCentreIds,List<Integer> deliveryStages,Pageable pageable);
-//
-//	
-	
 }
