2fc09ce1323255e6e79af6cdfa1100787e9d30f4
Badri Naraayanan M <badrinaraayanan.m@tcs.com>
2016-09-22 10:47:18 +0530

    delivery master repository

--- Destination/src/main/java/com/tcs/destination/data/repository/AuditDeliveryMasterRepository.java
+++ Destination/src/main/java/com/tcs/destination/data/repository/AuditDeliveryMasterRepository.java
@@ -12,8 +12,8 @@ import com.tcs.destination.bean.AuditDeliveryMasterT;
 @Repository
 public interface AuditDeliveryMasterRepository extends CrudRepository<AuditDeliveryMasterT, Integer>{ 
 	
-	List<AuditDeliveryMasterT> findByOldDeliveryMasterId(Integer engId);
+	List<AuditDeliveryMasterT> findByDeliveryMasterId(Integer engId);
 	
-	@Query(value = "select * from audit_delivery_master_t  where (operation_type  =1 or old_delivery_stage <> new_delivery_stage) and old_delivery_master_id = :engId ORDER BY created_modified_datetime asc", nativeQuery = true)
+	@Query(value = "select * from audit_delivery_master_t  where (operation_type  =1 or old_delivery_stage <> new_delivery_stage) and delivery_master_id = :engId ORDER BY created_modified_datetime asc", nativeQuery = true)
 	List<AuditDeliveryMasterT> getDeliveryCodeChanges(@Param("engId") Integer engId);
 }
