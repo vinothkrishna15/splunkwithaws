c7b4afaf28bf7ef84113ac2a7570f1d2fef9e93e
K Achari <k.achari@tcs.com>
2016-10-04 18:45:50 +0530

    Smart search SIT issue fixed

--- Destination/src/main/java/com/tcs/destination/data/repository/DeliveryMasterRepository.java
+++ Destination/src/main/java/com/tcs/destination/data/repository/DeliveryMasterRepository.java
@@ -15,7 +15,7 @@ public interface DeliveryMasterRepository extends JpaRepository<DeliveryMasterT,
 	/*------------------Start of delivery smart search queries-------------------*/
 	
 	@Query(value="SELECT * FROM delivery_master_t where delivery_centre_id in (SELECT delivery_centre_id FROM delivery_centre_t where delivery_cluster_id in ("
-			+ " SELECT delivery_cluster_id FROM delivery_cluster_t WHERE delivery_cluster_head=(:userId))) AND "
+			+ " SELECT delivery_cluster_id FROM delivery_cluster_t WHERE delivery_cluster_head=(:userId))  OR delivery_centre_id=-1) AND "
 			+ " UPPER(opportunity_id) like UPPER(:term) ORDER BY modified_datetime DESC LIMIT CASE WHEN :getAll THEN null ELSE 3 END",nativeQuery=true)
 	List<DeliveryMasterT> searchDeliveryClusterDetailsById(@Param("term") String term,
 			@Param("getAll") boolean getAll, @Param("userId") String userId);
@@ -31,7 +31,7 @@ public interface DeliveryMasterRepository extends JpaRepository<DeliveryMasterT,
 			@Param("getAll") boolean getAll, @Param("userId") String userId);
 
 	@Query(value="SELECT * FROM delivery_master_t where delivery_centre_id in (SELECT delivery_centre_id FROM delivery_centre_t where delivery_cluster_id in ("
-			+ " SELECT delivery_cluster_id FROM delivery_cluster_t WHERE delivery_cluster_head=(:userId)) AND UPPER(delivery_centre) like UPPER(:term)) "
+			+ " SELECT delivery_cluster_id FROM delivery_cluster_t WHERE delivery_cluster_head=(:userId)) AND UPPER(delivery_centre) like UPPER(:term)  OR delivery_centre_id=-1) "
 			+ " ORDER BY modified_datetime DESC LIMIT CASE WHEN :getAll THEN null ELSE 3 END", nativeQuery=true)
 	List<DeliveryMasterT> searchDeliveryClusterDetailsByDeliveryCentres(@Param("term") String term,
 			@Param("getAll") boolean getAll, @Param("userId") String userId);
@@ -48,7 +48,7 @@ public interface DeliveryMasterRepository extends JpaRepository<DeliveryMasterT,
 			@Param("getAll") boolean getAll, @Param("userId") String userId);
 
 	@Query(value="SELECT * FROM delivery_master_t where delivery_centre_id in (SELECT delivery_centre_id FROM delivery_centre_t WHERE delivery_cluster_id in ("
-			+ " SELECT delivery_cluster_id FROM delivery_cluster_t WHERE delivery_cluster_head=(:userId))) AND opportunity_id IN (SELECT opportunity_id FROM opportunity_t WHERE customer_id IN ("
+			+ " SELECT delivery_cluster_id FROM delivery_cluster_t WHERE delivery_cluster_head=(:userId)) OR delivery_centre_id=-1) AND opportunity_id IN (SELECT opportunity_id FROM opportunity_t WHERE customer_id IN ("
 			+ " SELECT customer_id from customer_master_t WHERE UPPER(customer_name) like UPPER(:term))) ORDER BY modified_datetime DESC LIMIT CASE WHEN :getAll THEN null ELSE 3 END ", nativeQuery=true) 
 	List<DeliveryMasterT> searchDeliveryClusterDetailsByCustomerName(@Param("term") String term,
 			@Param("getAll") boolean getAll, @Param("userId") String userId);
