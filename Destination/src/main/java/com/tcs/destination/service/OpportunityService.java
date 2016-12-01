b52a764d7ed43b56b6b67bb00092b91665102284
Manikandan Muthukrishnan <manikandan.5@tcs.com>
2016-12-01 11:30:54 +0530

    Opp Dashboard based on privilege

--- Destination/src/main/java/com/tcs/destination/service/OpportunityService.java
+++ Destination/src/main/java/com/tcs/destination/service/OpportunityService.java
@@ -3674,7 +3674,8 @@ public class OpportunityService {
 			return asyncJobRequest;
 		}
 	
-	public List<OpportunityT> getOpportunitiesBasedOnPrivileges(Date fromDate, Date toDate) throws Exception {
+	public PaginatedResponse getOpportunitiesBasedOnPrivileges(Date fromDate, Date toDate) throws Exception {
+		PaginatedResponse response = new PaginatedResponse();
 		List<OpportunityT> opportunityTs = Lists.newArrayList();
 		UserT currentUser = DestinationUtils.getCurrentUserDetails();
 		String userId = currentUser.getUserId();
@@ -3706,7 +3707,8 @@ public class OpportunityService {
 				opp.setDigitalDealValue(dealValueInUsd.intValue());
 			}
 		}
-		return opportunityTs;
+		response.setOpportunityTs(opportunityTs);
+		return response;
 		
 	}
 	
