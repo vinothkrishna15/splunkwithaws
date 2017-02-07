2938a6dac5cc7398afe7310f81b0afe832f67e2a
Vinoth Kumar <vinothkumar.k2@tcs.com>
2017-02-07 18:44:18 +0530

    Service to Upload Logo for Group Customer and Competitor.

--- Destination/src/main/java/com/tcs/destination/service/DataProcessingService.java
+++ Destination/src/main/java/com/tcs/destination/service/DataProcessingService.java
@@ -3,6 +3,9 @@ package com.tcs.destination.service;
 import static com.tcs.destination.utils.Constants.FILE_DIR_SEPERATOR;
 import static com.tcs.destination.utils.Constants.UPLOAD;
 
+import java.io.File;
+
+import org.apache.commons.io.FilenameUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +13,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.tcs.destination.bean.CompetitorMappingT;
 import com.tcs.destination.bean.DataProcessingRequestT;
+import com.tcs.destination.bean.GroupCustomerT;
 import com.tcs.destination.bean.Status;
+import com.tcs.destination.data.repository.CompetitorRepository;
 import com.tcs.destination.data.repository.DataProcessingRequestRepository;
+import com.tcs.destination.data.repository.GroupCustomerRepository;
 import com.tcs.destination.data.repository.UserRepository;
 import com.tcs.destination.enums.EntityType;
 import com.tcs.destination.enums.RequestStatus;
@@ -36,8 +43,16 @@ public class DataProcessingService {
 	@Autowired
 	private DataProcessingRequestRepository dataProcessingRequestRepository;
 	
+	@Autowired
+	private CompetitorRepository competitorRepository;
+	
+	@Autowired GroupCustomerRepository groupCustomerRepository;
+	
 	@Value("${fileserver.path}")
 	private String fileServerPath;
+	
+	@Value("${fileserver.logoPath}")
+	private String logoServerPath;
 
 	/**
 	 * This method is used to save the upload requests being provided for batch operations
@@ -184,6 +199,99 @@ public class DataProcessingService {
 		status.setStatus(Status.SUCCESS, "Download request is submitted successfully");
 		logger.debug("End :saveDownloadRequest method of DataProcessing Service");
 		return status;
+	}
+	
+	/**
+	 * Method to read the files from the folder to Update the logo column in
+	 * respective tables for competitor and Group customer
+	 * 
+	 * @return status - message and code corresponding to the action
+	 */
+
+	public Status readAndUploadLogo() {
+		Status status = new Status();
+		File directory = new File(logoServerPath);
+		// get all the files from a directory
+
+		File[] fList = directory.listFiles();
+		for (File file : fList) {
+			if (file!= null && file.isDirectory()) {
+				readAndSaveLogoFiles(file.getAbsolutePath(), status);
+			} else {
+				status.setStatus(Status.FAILED, "No Valid Directory");
+			}
+		}
+		return status;
+	}
+
+	/**
+	 * Method called to check the folder for competitor and customer and to load
+	 * the logo files as byte array accordingly.
+	 * 
+	 * @param directoryName
+	 * @param status
+	 */
+	private void readAndSaveLogoFiles(String directoryName,
+			Status status) {
+		File directory = new File(directoryName);
+		// get all the files from a directory
+		File[] fList = directory.listFiles();
+		for (File file : fList) {
+			if (file!= null && file.isFile()) {
+				String fileNameWithOutExt = FilenameUtils.removeExtension(file
+						.getName());
+				if (file.getAbsolutePath().contains("COMPETITOR")) {
+					updateCompetitorLogo(fileNameWithOutExt, file.getName(),
+							status);
+				} else if (file.getAbsolutePath().contains("GROUP_CUSTOMER")) {
+					updateGroupCustomerLogo(fileNameWithOutExt, file.getName(),
+							status);
+				} else {
+
+				}
+			}
+		}
+	}
+
+	/**
+	 * Method called to update group customer logo.
+	 * @param fileNameWithOutExt
+	 * @param fileName
+	 * @param status
+	 */
+	private void updateGroupCustomerLogo(String fileNameWithOutExt,
+			String fileName, Status status) {
+		GroupCustomerT oldObject = groupCustomerRepository
+				.findOne(fileNameWithOutExt);
+		if (oldObject != null) {
+			GroupCustomerT update = new GroupCustomerT();
+			update.setGroupCustomerName(oldObject.getGroupCustomerName());
+			update.setLogo(fileName.getBytes());
+			groupCustomerRepository.save(update);
+			status.setStatus(Status.SUCCESS, "Logo Uploaded successfully");
+		}
+
+	}
+
+	/**
+	 * Method called to update competitor logo.
+	 * @param fileNameWithOutExt
+	 * @param fileName
+	 * @param status
+	 * @param oldObject
+	 * 
+	 */
+	private void updateCompetitorLogo(String fileNameWithOutExt,
+			String fileName, Status status) {
+		CompetitorMappingT oldObject = competitorRepository
+				.findOne(fileNameWithOutExt);
+		if (oldObject != null) {
+			CompetitorMappingT update = new CompetitorMappingT();
+			update.setCompetitorName(oldObject.getCompetitorName());
+			update.setLogo(fileName.getBytes());
+			competitorRepository.save(update);
+			status.setStatus(Status.SUCCESS, "Logo Uploaded successfully");
+		}
 	}
 
 }
